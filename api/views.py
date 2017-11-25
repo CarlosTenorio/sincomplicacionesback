fe16df12a2c95173bacc7ca50c8fb479441a1404
CarlosTenorio <carlostenorioperez@hotmail.com>
2017-11-25 17:45:32 +0100

    Add athentication and permissions config

--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny,
+    IsAuthenticatedOrReadOnly
 
 from api.models import UserExtended, Province, City
 from api.serializers import UserSerializer, CitySerializer, ProvinceSerializer
 
+#############################
+# Default authentication #
+# IsAuthenticatedOrReadOnly #
+#############################
 
 class UserList(viewsets.ModelViewSet):
     queryset = UserExtended.objects.all()
@@ -10,10 +16,14 @@ class UserList(viewsets.ModelViewSet):
 
 
 class CityList(viewsets.ModelViewSet):
+    permission_classes = (AllowAny,)
+
     queryset = City.objects.all()
     serializer_class = CitySerializer
-    
+
 
 class ProvinceList(viewsets.ModelViewSet):
+    permission_classes = (AllowAny,)
+
     queryset = Province.objects.all()
     serializer_class = ProvinceSerializer
